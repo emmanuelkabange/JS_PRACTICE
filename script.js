7b4d1c632ba22a69dc3f698126e73b386ac3f559
Emmanuel Kabange <emmanuelwelek@gmail.com>
2024-07-28 14:38:42 +0000

    javascript code added

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const image = document.getElementById("image");
 
+function addHoverEffect() {
+    image.classList.add("hovered");
+}
+
+function removeHoverEffect() {
+    image.classList.remove("hovered");
+}
+
+image.addEventListener("mouseover", addHoverEffect);
+image.addEventListener("mouseout", removeHoverEffect);
